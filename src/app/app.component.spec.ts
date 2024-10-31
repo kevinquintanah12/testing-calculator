76a705000de62771d2f415ca854c7d7bde15997f
gersonvidal <gtotrac@gmail.com>
2024-10-30 19:30:04 -0600

    added missing imports and span for app component and passed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,36 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { UiComponent } from "./ui/ui.component";
+import { FormsModule } from "@angular/forms";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, UiComponent],
+      imports: [
+        FormsModule
+      ]
     }).compileComponents();
   }));
 
-  xit('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  xit(`should have as title 'testing-calculator'`, () => {
+  it(`should have as title 'testing-calculator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('testing-calculator');
+    expect(app.title).toEqual("testing-calculator");
   });
 
-  xit('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('testing-calculator app is running!');
+    expect(compiled.querySelector(".content span").textContent).toContain(
+      "testing-calculator app is running!"
+    );
   });
 });
